c030afa263ea353b56c75edc568ce9217af55c36
Alfan Lukeyan Rizki <alfanlukeyanrizki@gmail.com>
2023-12-22 16:39:38 +0700

    fix: Longest Increasing Subsequence algorithm

--- largest-monotonically.py
+++ largest-monotonically.py
@@ -1,29 +1,46 @@
-# Import the bisect_left function from the bisect module
-from bisect import bisect_left
+class LongestIncreasingSubsequence:
+    def __init__(self, sequence):
+        self.sequence = sequence
+        self.length_of_lis = 0
+        self.subsequence_lengths = []
+        self.subsequence_indices = []
 
-# Define the function for finding the Largest Monotonically Increasing Subsequence (LMIS)
-def LMIS(arr):
-    # Initialize the list for storing the LMIS
-    lis = [0]*len(arr)
-    # Initialize the length of the LMIS
-    length = 0
+    def compute_longest_increasing_subsequence(self):
+        sequence_length = len(self.sequence)
+        self.subsequence_indices = [0] * sequence_length
+        self.subsequence_lengths = [-1] * (sequence_length + 1)
+        self.subsequence_lengths[0] = -1
 
-    # Iterate over the input array
-    for i in range(len(arr)):
-        # Find the position where arr[i] can be inserted in lis
-        pos = bisect_left(lis, arr[i], 0, length)
-        # Update the element at the found position
-        lis[pos] = arr[i]
-        # If arr[i] is larger than all elements in lis, increase the length of the LMIS
-        if pos == length:
-            length += 1
+        for i in range(sequence_length):
+            lower_bound = 1
+            upper_bound = self.length_of_lis + 1
 
-    # Return the length of the LMIS
-    return length
+            while lower_bound < upper_bound:
+                mid = lower_bound + (upper_bound - lower_bound) // 2
 
-# Read the number of elements
-n = int(input())
-# Read the elements
-arr = list(map(int, input().split()))
-# Print the length of the LMIS
-print(LMIS(arr))
+                if self.sequence[self.subsequence_lengths[mid]] >= self.sequence[i]:
+                    upper_bound = mid
+                else:
+                    lower_bound = mid + 1
+
+            new_length = lower_bound
+            self.subsequence_indices[i] = self.subsequence_lengths[new_length - 1]
+            self.subsequence_lengths[new_length] = i
+
+            if new_length > self.length_of_lis:
+                self.length_of_lis = new_length
+
+        lis = [0] * self.length_of_lis
+        k = self.subsequence_lengths[self.length_of_lis]
+        for j in range(self.length_of_lis - 1, -1, -1):
+            lis[j] = self.sequence[k]
+            k = self.subsequence_indices[k]
+
+        return lis, self.length_of_lis
+
+if __name__ == "__main__":
+    sequence = [4, 1, 13, 7, 0, 2, 8, 11, 3]
+    lis_calculator = LongestIncreasingSubsequence(sequence)
+    longest_subsequence, length = lis_calculator.compute_longest_increasing_subsequence()
+    print("Longest Increasing Subsequence: ", longest_subsequence)
+    print("Length of Longest Increasing Subsequence: ", length)
